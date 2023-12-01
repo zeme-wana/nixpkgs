4300b00e1bd5c3123ce518a0eef669290644fcf2
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-12-01 18:26:05 +0100

    phpunit: 10.5.0 -> 10.5.1
    (cherry picked from commit 3994688356e7ea9f6eec912a8e550b67d26f8300)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18339,8 +18339,6 @@ with pkgs;
 
   karma-runner = callPackage ../development/tools/karma-runner { };
 
-  phpunit = callPackage ../development/tools/misc/phpunit { };
-
   teller = callPackage ../development/tools/teller { };
 
   yakut = python3Packages.callPackage ../development/tools/misc/yakut { };
