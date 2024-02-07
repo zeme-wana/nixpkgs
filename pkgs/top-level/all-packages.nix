a3a727678a49221dbb9d190376b490a1112cfd08
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-02-07 20:11:36 +0100

    pdf-sign: fix version name, simplify installPhase, move to pkgs/by-name
    (cherry picked from commit 73f6bc2c4e0b3dbcd82017a4394926121c67a8ee)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12017,8 +12017,6 @@ with pkgs;
 
   pdf-quench = callPackage ../applications/misc/pdf-quench { };
 
-  pdf-sign = callPackage ../tools/graphics/pdf-sign { };
-
   pdfarranger = callPackage ../applications/misc/pdfarranger { };
 
   briss = callPackage ../tools/graphics/briss { };
